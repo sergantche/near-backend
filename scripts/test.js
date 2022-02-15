8eb4a2e9b54ff5af2932ce256e23acae19287c9b
SergantChe <sergei.alex.kozlov@gmail.com>
2022-02-15 22:28:23 +0300

    Finish dev tests

--- scripts/test.js
+++ scripts/test.js
@@ -14,19 +14,30 @@ const main = async () => {
     process.env.CONTRACT_NAME ||
     fs.readFileSync("./neardev/dev-account").toString();
 
-  console.log("Try to craft a new hero ...");
+  // Get token supply
+  console.log("Get total token supply and for sergantche.testnet ...");
+  sh.exec(`near view ${contractName} nft_total_supply`);
   sh.exec(
     `near view ${contractName} nft_supply_for_owner '{"account_id": "sergantche.testnet"}'`
   );
+
+  // Mint and send nft
+  console.log("Try to craft a new hero ...");
   let res = await sh.exec(
     `near call ${contractName} craft_new_hero '{"username": "sergantche.testnet"}' --deposit-yocto 9000000000000000000000 --account-id ${contractName} --gas 300000000000000`
   );
   const tokenId = getResult(res);
 
   // Get new hero statistics
-  res = await sh.exec(
-    `near view ${contractName} get_stats '{"token_id": "${tokenId}"}'`
+  console.log("Try to get hero stats ...");
+  sh.exec(`near view ${contractName} get_stats '{"token_id": "${tokenId}"}'`);
+
+  // Update hero statistics and get data again
+  console.log("Try to update hero stats ...");
+  sh.exec(
+    `near call ${contractName} update_hero_stats '{"token_id": "${tokenId}", "new_stars": 5, "new_experience": 6, "new_maximum_level": 7}' --account-id ${contractName} --gas 30000000000000`
   );
+  sh.exec(`near view ${contractName} get_stats '{"token_id": "${tokenId}"}'`);
 
   // Exit script with the same code as the build command
   process.exit();
