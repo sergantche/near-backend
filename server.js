44fc5397a0b1ec49f0dd1701049bfba250968f45
SergantChe <sergei.alex.kozlov@gmail.com>
2022-02-16 02:45:21 +0300

    give the server access from the account

--- server.js
+++ server.js
@@ -1,6 +1,67 @@
 const express = require("express");
+const http = require("http");
+const nacl = require("tweetnacl");
+nacl.util = require("tweetnacl-util");
+const cors = require("cors");
+const fs = require("fs");
+const nearAPI = require("near-api-js");
+const getConfig = require("./config/near");
+
+// Instantiate server with WS support
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+// const io = require("socket.io")(server, {
+//   cors: true,
+//   origins: ["*"],
+// });
+
+const nearConfig = getConfig(process.env.APP_ENV || "development");
+const { nodeUrl, networkId, contractName } = nearConfig;
+const contractMethods = {
+  changeMethods: ["craft_new_hero", "update_hero_stats"],
+  viewMethods: ["get_stats"],
+};
+
+const {
+  keyStores: { InMemoryKeyStore },
+  Near,
+  Account,
+  Contract,
+  KeyPair,
+  utils: {
+    format: { parseNearAmount },
+  },
+} = nearAPI;
+
+// Load credentials
+console.log("Loading Credentials:\n", `./creds/${contractName}.json`);
+const credentials = JSON.parse(fs.readFileSync(`./creds/${contractName}.json`));
+
+const keyStore = new InMemoryKeyStore();
+keyStore.setKey(
+  networkId,
+  contractName,
+  KeyPair.fromString(credentials.private_key)
+);
+const near = new Near({
+  networkId,
+  nodeUrl,
+  deps: { keyStore },
+});
+const { connection } = near;
+const contractAccount = new Account(connection, contractName);
+contractAccount.addAccessKey = (publicKey) =>
+  contractAccount.addKey(
+    publicKey,
+    contractName,
+    contractMethods.changeMethods,
+    parseNearAmount("0.1")
+  );
+
+const contract = new Contract(contractAccount, contractName, contractMethods);
+
+///   API   ///
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
