46c88e77476fc175be114aaa7f8fd3a5cdb6f8ca
SergantChe <sergei.alex.kozlov@gmail.com>
2022-02-15 20:15:07 +0300

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -3,23 +3,34 @@
 // makes it easy to use *NIX-style scripting (which works on Linux distros,
 // macOS, and Unix systems) on Windows as well.
 const sh = require("shelljs");
-const contractName =
-  process.env.CONTRACT_NAME ||
-  fs.readFileSync("./neardev/dev-account").toString();
 
-console.log("Try to craft a new hero ...");
-sh.exec(
-  `near view ${contractName} nft_supply_for_owner '{"account_id": "sergantche.testnet"}'`
-);
-sh.exec(
-  `near call ${contractName} craft_new_hero '{"username": "sergantche.testnet"}' --deposit-yocto 9000000000000000000000 --account-id ${contractName} --gas 300000000000000`
-);
-sh.exec(
-  `near view ${contractName} nft_supply_for_owner '{"account_id": "sergantche.testnet"}'`
-);
-sh.exec(
-  `near view ${contractName} nft_tokens_for_owner '{"account_id": "sergantche.testnet"}'`
-);
+// Parse near call response and return result of contract function call
+const getResult = (res) => {
+  return res.stdout.split(/\r?\n/).slice(-2, -1)[0];
+};
 
-// exit script with the same code as the build command
-process.exit();
+const main = async () => {
+  const contractName =
+    process.env.CONTRACT_NAME ||
+    fs.readFileSync("./neardev/dev-account").toString();
+
+  console.log("Try to craft a new hero ...");
+  sh.exec(
+    `near view ${contractName} nft_supply_for_owner '{"account_id": "sergantche.testnet"}'`
+  );
+  let res = await sh.exec(
+    `near call ${contractName} craft_new_hero '{"username": "sergantche.testnet"}' --deposit-yocto 9000000000000000000000 --account-id ${contractName} --gas 300000000000000`
+  );
+  const tokenId = getResult(res);
+
+  // Get new hero statistics
+  res = await sh.exec(
+    `near view ${contractName} get_stats '{"token_id": "${tokenId}"}'`
+  );
+
+  // Exit script with the same code as the build command
+  process.exit();
+};
+
+// Run tests
+main();
